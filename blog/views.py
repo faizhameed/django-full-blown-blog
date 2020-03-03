f3dcee1abfbee5e8c21bf53b0cfa9599b78d447d
faizhameed <faizftb@gmail.com>
2020-03-03 15:54:13 +0530

    Added templates to blog app

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[{
+    'author':"faiz hameed",
+    'title':"django dev",
+    'content':"django content for new website",
+    'date_posted':"March 03 2020"
+},
+{
+    'author':"muhammed saif",
+    'title':"dbackend",
+    'content':"django content said",
+    'date_posted':"April 13 2020"
+}]
+
 def home(request):
-    return HttpResponse('<h1>Blog Page</h1>')
+    context={
+       "posts":posts
+    }
+    return render(request,"blog/home.html",context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About<h1>')
-
+    return render(request,"blog/about.html")
 # Create your views here.
