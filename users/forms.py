3001765250c2816e616b4e0472147c3b5a6e0c47
faizhameed <faizftb@gmail.com>
2020-03-16 11:20:54 +0530

    prev commits

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField() #to have an extra field of email
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
